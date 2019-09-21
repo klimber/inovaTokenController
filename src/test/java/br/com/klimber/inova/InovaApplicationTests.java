f5256eec1fe2bb8855bd9d6c952ad22c7254e1fb
Allan Krueger <klimber.mail@gmail.com>
2019-09-21 15:17:16 -0300

    Initial Commit
    Spring initializer app with sample restcontroller

---
+++ src/test/java/br/com/klimber/inova/InovaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.klimber.inova;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InovaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
