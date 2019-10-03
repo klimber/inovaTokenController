f79b8adb486edc9d4c42a6ef07645f71a96fd216
Allan Krueger <allan.krueger@projuris.com.br>
2019-10-02 21:39:58 -0300

    WIP - Refactor pbiController

--- src/main/java/br/com/klimber/inova/endpoint/PbiController.java
+++ src/main/java/br/com/klimber/inova/endpoint/PbiController.java
@@ -1,78 +1,40 @@
 package br.com.klimber.inova.endpoint;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.klimber.inova.model.EmbedToken;
 import br.com.klimber.inova.model.Group;
-import br.com.klimber.inova.model.PbiRestResponse;
 import br.com.klimber.inova.model.Report;
-import br.com.klimber.inova.service.AzureTokenService;
+import br.com.klimber.inova.service.PbiService;
 
 @RestController
 public class PbiController {
 
 	@Autowired
-	private AzureTokenService azureTokenService;
-	private RestTemplate restTemplate = new RestTemplate();
-	private HttpHeaders headers = new HttpHeaders();
-	private HttpEntity<String> request;
-	private static PbiRestResponse<Group> groups;
-	private static PbiRestResponse<Report> reports;
-	private EmbedToken embedToken;
-	private ObjectMapper mapper = new ObjectMapper();
+	private PbiService pbiService;
 
 	@GetMapping("/pbi/groups")
-	public PbiRestResponse<Group> getGroups() {
-		String url = "https://api.powerbi.com/v1.0/myorg/groups";
-		String azureToken = azureTokenService.getToken();
-		headers.setBearerAuth(azureToken);
-		request = new HttpEntity<>(headers);
-		groups = restTemplate
-				.exchange(url, HttpMethod.GET, request, new ParameterizedTypeReference<PbiRestResponse<Group>>() {
-				}).getBody();
-		return groups;
+	public List<Group> getGroups() {
+		return pbiService.getGroups();
 	}
 
-	@GetMapping("/pbi/reports")
-	public PbiRestResponse<Report> getReportsInGroup(@RequestParam String groupId) {
-		String url = "https://api.powerbi.com/v1.0/myorg/groups/" + groupId + "/reports";
-		String azureToken = azureTokenService.getToken();
-		headers.setBearerAuth(azureToken);
-		request = new HttpEntity<>(headers);
-		reports = restTemplate
-				.exchange(url, HttpMethod.GET, request, new ParameterizedTypeReference<PbiRestResponse<Report>>() {
-				}).getBody();
-		return reports;
+	@GetMapping("/pbi/groups/{groupId}/reports")
+	public List<Report> getReportsInGroup(@PathVariable("groupId") String groupId) {
+		return pbiService.getReportsInGroup(groupId);
 	}
 
-	@GetMapping("/pbi/embedToken")
+	@GetMapping("/pbi/groups/{groupId}/reports/{reportId}/GenerateToken")
 	public EmbedToken postForEmbedToken(@RequestParam String groupId, @RequestParam String reportId)
 			throws JsonProcessingException {
-		String url = "https://api.powerbi.com/v1.0/myorg/groups/" + groupId + "/reports/" + reportId + "/GenerateToken";
-		String azureToken = azureTokenService.getToken();
-		headers.setBearerAuth(azureToken);
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		Map<String, String> body = new HashMap<>();
-		body.put("accessLevel", "View");
-		body.put("allowSaveAs", "false");
-		request = new HttpEntity<String>(mapper.writeValueAsString(body), headers);
-		embedToken = restTemplate.postForObject(url, request, EmbedToken.class);
-		return embedToken;
+		return pbiService.getReportEmbedToken(groupId, reportId);
 	}
 
 }
---
+++ src/main/java/br/com/klimber/inova/service/PbiService.java
@@ -0,0 +1,71 @@
+package br.com.klimber.inova.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.klimber.inova.model.EmbedToken;
+import br.com.klimber.inova.model.Group;
+import br.com.klimber.inova.model.PbiRestResponse;
+import br.com.klimber.inova.model.Report;
+
+@Service
+public class PbiService {
+
+	@Autowired
+	private AzureTokenService azureTokenService;
+	private static RestTemplate restTemplate = new RestTemplate();
+	private static ObjectMapper mapper = new ObjectMapper();
+
+	public List<Group> getGroups() {
+		String url = "https://api.powerbi.com/v1.0/myorg/groups";
+		String azureToken = azureTokenService.getToken();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth(azureToken);
+		HttpEntity<Object> request = new HttpEntity<>(headers);
+		List<Group> groups = restTemplate
+				.exchange(url, HttpMethod.GET, request, new ParameterizedTypeReference<PbiRestResponse<Group>>() {
+				}).getBody().getValue();
+		return groups;
+	}
+
+	public List<Report> getReportsInGroup(String groupId) {
+		String url = "https://api.powerbi.com/v1.0/myorg/groups/" + groupId + "/reports";
+		String azureToken = azureTokenService.getToken();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth(azureToken);
+		HttpEntity<Object> request = new HttpEntity<>(headers);
+		List<Report> reports = restTemplate
+				.exchange(url, HttpMethod.GET, request, new ParameterizedTypeReference<PbiRestResponse<Report>>() {
+				}).getBody().getValue();
+		return reports;
+	}
+
+	public EmbedToken getReportEmbedToken(@RequestParam String groupId, @RequestParam String reportId)
+			throws JsonProcessingException {
+		String url = "https://api.powerbi.com/v1.0/myorg/groups/" + groupId + "/reports/" + reportId + "/GenerateToken";
+		String azureToken = azureTokenService.getToken();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth(azureToken);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		Map<String, String> body = new HashMap<>();
+		body.put("accessLevel", "View");
+		body.put("allowSaveAs", "false");
+		HttpEntity<String> request = new HttpEntity<String>(mapper.writeValueAsString(body), headers);
+		return restTemplate.postForObject(url, request, EmbedToken.class);
+	}
+
+}
