4b01953244eecfae6250a3347ae08750bcf60feb
Allan Krueger <allan.krueger@projuris.com.br>
2019-10-03 13:37:25 -0300

    Fix GenerateToken endpoint
    The parameters are PathVariables, nov Requestparams

--- src/main/java/br/com/klimber/inova/endpoint/PbiController.java
+++ src/main/java/br/com/klimber/inova/endpoint/PbiController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -32,8 +31,8 @@ public class PbiController {
 	}
 
 	@GetMapping("/pbi/groups/{groupId}/reports/{reportId}/GenerateToken")
-	public EmbedToken postForEmbedToken(@RequestParam String groupId, @RequestParam String reportId)
-			throws JsonProcessingException {
+	public EmbedToken postForEmbedToken(@PathVariable("groupId") String groupId,
+			@PathVariable("reportId") String reportId) throws JsonProcessingException {
 		return pbiService.getReportEmbedToken(groupId, reportId);
 	}
 
