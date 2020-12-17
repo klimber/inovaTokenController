a4a9c46df76c64e58d194c5f5fcd08ccd4f36bf9
Allan Krueger <allan.krueger@projuris.com.br>
2020-12-17 16:00:20 -0300

    Adicionado remoção de logs antigos
    Adicionado um job que fará a remoção de entradas antigas de log. É possível definir a quantidade de dias de idade após a qual o log deve ser excluído através do argumento REMOVE_LOG_AFTER_DAYS. Caso o argumento não seja definido, será assumido um padrão de 90 dias.

--- src/main/java/br/com/klimber/inova/repository/AccessLogRepository.java
+++ src/main/java/br/com/klimber/inova/repository/AccessLogRepository.java
@@ -1,11 +1,19 @@
 package br.com.klimber.inova.repository;
 
+import java.sql.Timestamp;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.klimber.inova.model.AccessLog;
 
 @Repository
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
+
+	@Modifying
+	@Query("delete from AccessLog where date < ?1")
+	public void deleteOlderThanDate(Timestamp date);
 
 }
--- src/main/java/br/com/klimber/inova/utils/ScheduledTasks.java
+++ src/main/java/br/com/klimber/inova/utils/ScheduledTasks.java
@@ -1,10 +1,13 @@
 package br.com.klimber.inova.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.klimber.inova.service.AccessLogService;
 import br.com.klimber.inova.service.PbiService;
 import lombok.extern.java.Log;
 
@@ -16,21 +19,26 @@ public class ScheduledTasks {
 	@Autowired
 	private PbiService pbiService;
 
+	@Autowired
+	private AccessLogService logService;
+	@Value("${app.remove-log-after-days}")
+	private int remove_log_days;
+
+	@Transactional
 	@Scheduled(fixedRate = 3600000) // Every 1 hour
 	private void update() {
-		updateGroups();
-		updateReports();
-	}
-
-	private void updateGroups() {
-		log.info("Started update groups job");
+		log.info("Started 'update groups' job");
 		pbiService.updateGroups();
-		log.info("Finished update groups job");
+		log.info("Finished 'update groups' job");
+		log.info("Started 'update reports' job");
+		pbiService.updateReports();
+		log.info("Finished 'update reports' job");
 	}
 
-	private void updateReports() {
-		log.info("Started update reports job");
-		pbiService.updateReports();
-		log.info("Finished update reports job");
+	@Scheduled(fixedRate = 86400000)
+	private void deleteOldLogs() {
+		log.info("Started 'delete old logs' job");
+		logService.removeLogsOlderThan(remove_log_days);
+		log.info("Finished 'delete old logs' job");
 	}
 }
