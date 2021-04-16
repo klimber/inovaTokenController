413f7e035abfe51753e8905468c19412e27a1ef9
Allan Krueger <allan.krueger@projuris.com.br>
2021-04-16 17:51:06 -0300

    Make tests use test profile

--- src/test/java/br/com/klimber/inova/InovaApplicationTests.java
+++ src/test/java/br/com/klimber/inova/InovaApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.klimber.inova;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = { "test" })
 class InovaApplicationTests {
 
 	@Test
